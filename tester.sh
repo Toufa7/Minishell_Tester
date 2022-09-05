2c8a7905c774b985178e768d6c45b00ebbc026b8
PØM$ <90482193+Toufa7@users.noreply.github.com>
2022-09-05 10:48:59 +0100

    Adding New Option -a

--- tester.sh
+++ tester.sh
@@ -6,8 +6,6 @@
 #Email         	: abouchfa@student.42.fr                          #
 ###################################################################
 
-# $@ ==> $@ is nearly the same as $*, both meaning "all command line arguments". 
-
 RESET="\033[0m"
 BLACK="\033[30m"
 RED="\033[31m"
@@ -111,28 +109,38 @@ function	usage()
 	echo "\033[1;37m	-e	${NORMAL}Export"
 }
 
+function ProgressBar
+{
+    let progress=(${1}*100/${2}*100)/100
+    let done=(${progress}*4)/10
+    let left=40-$done
+    fill=$(printf "%${done}s")
+    empty=$(printf "%${left}s")
+    
+printf "\rCompiling: [${fill// /#}${empty// /-}] ${progress}%%"
 
+}
 
 function testing()
 {
 	# Put you Minishell's path down here
 	MINISHELL_PATH=".."
-	# Assuming based on the subject your program name is minishell
+	# Based on the subject your program name is minishell
 	# Simply Checking if you have an executable file called minishell
-	# test -x $MINISHELL_PATH/minishell
-	# if [ $? != 0 ];
-	# then
-	# {
-	# 	echo "Compiling\n"
-	# 	cd $MINISHELL_PATH ; make ; cd $OLDPWD
-	# 	i=1
-	# 	BAR='####################'
-	# 	for i in {1..20};
-	# 	do
-	# 	    echo -ne "\r${BAR:0:$i}"
-	# 	    sleep .1              
-	# 	done
-	# }
+	test -x $MINISHELL_PATH/minishell
+	if [ $? != 0 ];
+	then
+	{
+		cd $MINISHELL_PATH ; make ; cd $OLDPWD
+		start=1
+		end=10
+		for i in $(seq ${start} ${end})
+		do
+			sleep 0.1
+			ProgressBar ${i} ${end}
+		done
+		echo "\n"
+	}
 	fi
 	MINISHELL=$(echo $*	| $MINISHELL_PATH/minishell 2>&-)
 	MINISHELL_EXIT_STATUS=$?
@@ -513,5 +521,288 @@ if [[ $1 = "-c" ]]; then
 	testing cd ../$NONEXIT/Builtins
 
 	grade 35
+}
+fi
+
+if [[ $1 = "-a" ]]; then
+{
+	printf "%s$ECHO\n"
+
+	testing echo \'$\'
+	testing echo $abc$
+	testing eChO "\"$"\"	
+	testing echo \'\'$\'\'
+	testing echo "$_"
+	testing echo 					ls
+	testing echo '$1337' 
+	testing echo "\$1337"
+	testing echo "$0"
+	testing echo "$0"
+	testing 'e'"c"'h'o	"POMS"
+	testing echo echo
+	testing echo "			a"	
+	testing eCHO ''	
+	testing echo $	
+	testing echo $ $
+	testing echo	
+	testing echo wow		
+	testing echo Hello World	
+	testing echo "Salam Alikoum"		
+	testing echo Salam Alikoum		
+	testing echo 'Salam Alikoum'		
+	testing echo -n Salam Alikoum		
+	testing echo -nn Salam Alikoum	
+	testing echo -n -n -n Salam Alikoum
+	testing echo "-n" Salam Alikoum	
+	testing echo -n "-n" Salam Alikoum	
+	testing echo "-nnnn" Salam Alikoum
+	testing echo "-nnnn" -n Salam Alikoum
+	testing echo "-n -n -n"-n Salam Alikoum		
+	testing echo "-n '-n'" Salam Alikoum
+	testing echo $string" | sed -e "s/^$prefix//" -e "s/$suffix$//
+	testing echo $USER
+	testing echo "$USER"		
+	testing echo "'$USER'"		
+	testing echo " '$USER' "	
+	testing echo text"$USER"	
+	testing echo text"'$USER'" ' $USER '	
+	testing echo "Bensouda"   "$USER"    "$USER"
+	testing echo '              $USER          '		
+	testing echo               Bensouda "$USER"            "$USER"Bensouda	
+	testing echo ''''''''''$USER''''''''''	
+	testing echo """"""""$USER""""""""		
+	testing echo $USER'$USER'Once a gunner always a gunner $USER COYG      $USER ''	
+	testing echo $USER '' $USER $USER '' $USER '' $USER -n $USER	
+	testing echo "$USER""$USER""$USER"		
+	testing echo	guess whos back "$USER"
+	testing echo '"'$USER'"'
+	testing echo $USER " "		
+	testing echo "$USER""Users/$USER/file""'$USER'"'$USER'
+	testing echo "$USER$USER$USER"
+	testing echo '$USER'"$USER"'$USER'		
+	testing echo '"$USER"''$USER'"""$USER"	
+	testing echo " $USER  "'$PWD'
+	testing echo " $USER  \$ "'$PWD'		
+	testing echo $USER=4		
+	testing echo "${NOT_EXIT} POMS"		
+	testing echo "${USER} POMS"		
+	testing echo "$USER"		
+	testing echo $USER=POMS 
+	testing echo $USER
+	testing echo $?	
+	testing echo $PWD/file		
+	testing echo "$PWD/file"	
+	testing echo "Nothing" "Nothing$USER_ANA" ... "$USER.ANA"
+	testing echo "Nothing" "Nothing$USER9WHO" ... "$USER-9"
+	testing echo $PWD
+
+
+	printf "$PIPES"
+
+	testing "ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls"
+	testing "env | grep \"_=\""
+	testing "env | grep \"SHLVL\""
+	testing "echo oui | cat -e"
+	testing "echo oui | echo non | echo P0MS | grep oui"
+	testing "echo oui | echo non | echo P0MS | grep non"
+	testing "echo oui | echo non | echo P0MS | grep P0MS"
+	testing "cd .. | echo \"P0MS\""
+	testing "cd .. | echo \"P0MS\""
+	testing "cd / | echo \"P0MS\""
+	testing "cd .. | pwd"
+	testing "ifconfig | grep \":\""
+	testing "ifconfig | grep nothing"
+	testing "whoami | grep $USER"
+	testing "whoami | grep $USER > tmp/file"
+	testing "whoami | cat -e | cat -e > tmp/file		"
+	testing "cat Makefile | grep "FLAGS"	"
+	testing "cat Makefile | cat -e | cat -e"
+	testing "cat Makefile | grep "FLAGS" | grep "FLAGS" | cat -e	"
+	testing "export TEST=123 | cat -e | cat -e		"
+	testing "unset TEST | cat -e	"
+	testing "echo test | cat -e | cat -e | cat -e	"
+	testing "whereis ls | cat -e | cat -e > test		"
+	testing "echo test | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e"
+	testing "ls -la | grep ".""
+	testing "whereis grep > tmp/check" # Seg Fault use fsanitizer
+	testing "whereis grep > /tmp/check" # Seg Fault use fsanitizer
+	testing "ls -la > tmp/check"
+
+		printf "%s$EXIT\n"
+
+	testing exit 1337 1337
+	testing exit -1337 -24
+	testing exit 1337
+	testing exit 1337 53 68
+	testing exit 259
+	testing exit -12030
+	testing exit --1239312
+	testing exit ++++1203020103
+	testing exit +0
+	testing exit ++++++0
+	testing exit -----0
+	testing exit azerty
+	testing exit Benguerir
+	testing exit a
+	testing exit z
+	testing exit "1"
+	testing exit "2"
+	testing exit "+102"
+	testing exit "1230"
+	testing exit "+++1230"
+	testing exit "1"23
+	testing exit "2"32"32"
+	testing exit "'1337'"
+	testing exit '1337'"1337"1337
+	testing exit +'1337'"1337"1337
+	testing exit -'1337'"1337"1337
+	testing exit 922337203685477
+	testing exit 922337203685477
+	testing exit -4
+	testing exit 1
+	testing exit -1
+	testing exit 1337
+	testing exit 0
+	testing exit --000
+	testing exit +++++++000
+	testing exit ++++3193912939
+	testing exit ---31232103012
+	testing exit "Oh Darling"
+	testing exit echo
+	testing exit cd ..
+	testing exit 0 0
+	testing exit 1337 1337 1337 1337 1337
+	testing exit echo Bensouda
+	testing exit exit exit exit exit exit
+
+		printf "%s$EXPORT\n"
+
+	testing "env | grep \"_="\"
+	testing "export | grep \"SHLVL"\""
+	testing "export | grep \"OLDPWD"\""
+	testing "export | grep \"PWD"\"	
+	testing export $?
+	testing export TEST
+	testing export TEST=		
+	testing export TEST=123		
+	testing export ___TEST=123	
+	testing export --TEST=123	
+	testing export ""=""		
+	testing export ''=''		
+	testing export "="="="		
+	testing export '='='='		
+	testing export TE\\\ST=100	
+	testing export TE-ST=100	
+	testing export -TEST=100	
+	testing export TEST-=100	
+	testing export _TEST=100	
+	testing export TEST
+	testing export ==========	
+	testing export 1TEST=		
+	testing export TEST
+	testing export ""=""		
+	testing export TES=T=""		
+	testing export TE+S=T=""	
+	testing export TES\\\\T=123
+	testing export TES.T=123
+	testing export TES\\\$T=123
+	testing export TES\\\\T
+	testing export TES.T=123
+	testing export TES+T=123
+	testing export TES=T=123
+	testing export TES}T=123
+	testing export TES{T=123
+	testing export TES-T=123
+	testing export -TEST=123
+	testing export _TEST=123
+	testing export TES_T=123
+	testing export TEST_=123
+	testing export TE*ST=123
+	testing export TES#T=123
+	testing export TES@T=123
+	testing export TES!T=123
+	testing export TES$?T=123
+	testing export =============123
+	testing export +++++++=123
+	testing export ________=123
+	testing export export
+	testing export echo
+	testing export pwd
+	testing export cd
+	testing export export
+	testing export unset
+	testing export sudo
+	testing export TES^T=123
+	testing export TES!T=123
+	testing export TES\~T=123
+	testing export TEST+=100
+
+	printf "%s$UNSET\n"
+
+	testing unset doesntexist
+	testing unset PWD
+	testing unset PWD
+	testing unset OLDPWD
+	testing unset PATH
+	testing unset PATH
+	testing unset PATH
+	testing unset TES\\\\T
+	testing unset TES.T
+	testing unset TES+T
+	testing unset TES=T
+	testing unset TES}T
+	testing unset TES{T
+	testing unset TES-T
+	testing unset -TEST
+	testing unset _TEST
+	testing unset TES_T
+	testing unset TEST_
+	testing unset TE*ST
+	testing unset TES#T
+	testing unset TES@T
+	testing unset TES!T
+	testing unset TES$?T
+	testing unset ============
+	testing unset +++++++
+	testing unset ________
+	testing unset export
+	testing unset echo
+	testing unset pwd
+	testing unset cd
+	testing unset unset
+	testing unset sudo
+	testing unset TES^T
+	testing unset TES!T
+	testing unset TES\~T
+
+
+	printf "%s$CD\n"
+
+	testing cd 							
+	testing cd .. 						
+	testing cd . 						
+	testing cd /Users 					
+	testing cd // 						
+	testing cd '//' 					
+	testing cd ////// 					
+	testing cd ./././ 					
+	testing cd / 						
+	testing cd '/etc' 					
+	testing cd '/var' 					
+	testing cd "$PWD/prompt"			
+	testing cd "doesntexist"			
+	testing cd "doesntexist" 2>/dev/null
+	testing cd ../../..
+	testing cd ..						
+	testing cd ..						
+	testing cd ?						
+	testing cd +						
+	testing cd _						
+	testing cd bark bark				
+	testing cd '/'						
+	testing cd $PWD/file_tests			
+	testing cd ../$NONEXIT/Builtins
+
+	grade 257
 }
 fi
